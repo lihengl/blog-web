ebd057de0acd036e26fd479df4824fce1a920f8f
Li-Heng Liang <admin@lihengl.com>
2015-02-02 09:02:13 +0800

    Add external JS

--- component/Page.jsx
+++ component/Page.jsx
@@ -3,9 +3,9 @@ var React = require("react");
 var Page = React.createClass({
     render: function () {
         "use strict";
-        var reference = {
-            react: this.props.local ? "/bower_components/react/react.min.js" : "http://fb.me/react-0.12.2.min.js",
-            style: this.props.local ? "/static/blog." : "http://cdn.lihengl.com/blog/"
+        var external = {
+            lib: this.props.local ? "/bower_components/react/react.min.js" : "http://fb.me/react-0.12.2.min.js",
+            src: this.props.local ? "/static/blog." : "http://cdn.lihengl.com/blog/"
         };
         return <html lang="en-US">
             <head>
@@ -14,15 +14,17 @@ var Page = React.createClass({
                 <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
                 <meta name="format-detection" content="telephone=no"/>
                 <meta name="google" value="notranslate"/>
-                <link href={reference.style + this.props.version + ".min.css"} type="text/css" rel="stylesheet"/>
+                <link href={external.src + this.props.version + ".min.css"} type="text/css" rel="stylesheet"/>
                 <link href="/favicon.ico" type="image/x-icon" rel="shortcut icon"/>
                 <link href="/favicon.ico" type="image/x-icon" rel="icon"/>
                 <title>{this.props.title}</title>
             </head>
             <body>
+                <div id="bd"></div>
                 <h1>{this.props.title}</h1>
                 <input type="text" value="Hello, world!"/>
-                <script src={reference.react}></script>
+                <script src={external.lib}></script>
+                <script src={external.src + this.props.version + ".min.js"}></script>
             </body>
         </html>;
     }
