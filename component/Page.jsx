0f7dd73d9a8684507fd528d8d2ec99a692718cd1
LIHENG <admin@lihengl.com>
2015-02-20 22:49:47 +0800

    Improve naming

--- component/Page.jsx
+++ component/Page.jsx
@@ -3,7 +3,7 @@ var React = require("react");
 var Page = React.createClass({
     render: function () {
         "use strict";
-        var external = {
+        var ref = {
             lib: this.props.local ? "/bower_components/react/react.min.js" : "http://fb.me/react-0.12.2.min.js",
             src: this.props.local ? "/static/blog." : "http://cdn.lihengl.com/blog/"
         };
@@ -14,7 +14,7 @@ var Page = React.createClass({
                 <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
                 <meta name="format-detection" content="telephone=no"/>
                 <meta name="google" value="notranslate"/>
-                <link href={external.src + this.props.version + ".min.css"} type="text/css" rel="stylesheet"/>
+                <link href={ref.src + this.props.version + ".min.css"} type="text/css" rel="stylesheet"/>
                 <link href="/favicon.ico" type="image/x-icon" rel="shortcut icon"/>
                 <link href="/favicon.ico" type="image/x-icon" rel="icon"/>
                 <title>{this.props.title}</title>
@@ -23,8 +23,8 @@ var Page = React.createClass({
                 <h1>{this.props.title}</h1>
                 <input type="text" value="Hello, world!"/>
                 <div id="bd"></div>
-                <script src={external.lib}></script>
-                <script src={external.src + this.props.version + ".min.js"}></script>
+                <script src={ref.lib}></script>
+                <script src={ref.src + this.props.version + ".min.js"}></script>
             </body>
         </html>;
     }
