e7af0b6c679ce728a69d06c86ee1895cc69799dc
Li-Heng Liang <admin@lihengl.com>
2015-08-23 21:09:06 +0800

    Update version and README

--- webpack.config.js
+++ webpack.config.js
@@ -24,16 +24,21 @@ var config = {
         path: [__dirname, 'static_assets', pkg.version].join('/'),
         publicPath: ['http://localhost:8080/static_assets', pkg.version].join('/')
     },
-    plugins: [new webpack.NoErrorsPlugin()]
+    plugins: []
 };
 
 if (process.env.MODE === 'local') {
     config.entry.unshift('webpack/hot/only-dev-server');
     config.entry.unshift('webpack-dev-server/client?http://0.0.0.0:8080');
     config.module.loaders[1].loaders.unshift('react-hot');
+    config.plugins.push(new webpack.NoErrorsPlugin());
 } else {
     config.externals['react/addons'] = 'React';
-    config.plugins.unshift(new webpack.optimize.UglifyJsPlugin());
+    config.plugins.push(new webpack.optimize.OccurenceOrderPlugin());
+    config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+    config.plugins.push(new webpack.DefinePlugin({
+        'process.env': {'NODE_ENV': JSON.stringify('production')}
+    }));
 }
 
 module.exports = config;
